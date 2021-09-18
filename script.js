db96c7180cf232b3d7af3ce4bbe938d5bef1b454
narwhalios <porpoisegenb@gmail.com>
2021-09-18 10:17:28 +0800

    Base submission

--- script.js
+++ script.js
@@ -3,82 +3,106 @@
 // 3. The player picks the order of the dice they want. For example, if they wanted the number 63, they would specify that the 2nd dice goes first. You can choose how the player specifies dice order.
 // 4. After both players have rolled and chosen dice order, the player with the higher combined number wins.
 
-//Global for the state of the game
-var gameDiceRoll = "Dice roll for player";
-var gamePickDiceOrder = "Player pick dice order";
-var gameState = gameDiceRoll;
+//Global for the states of the game
+var gameDiceRollforPlayer1 = "Dice roll for player 1";
+var gameUserOrderforPlayer1 = "Player 1 pick dice order";
+var gameDiceRollforPlayer2 = "Dice roll for player 2";
+var gameUserOrderforPlayer2 = "Player 2 pick dice order";
+var compareNumbers = "Winning condition";
+
+var gameState = gameDiceRollforPlayer1;
+
+//Global for storing the number that the user selected
+var player01Number = [];
+var player02Number = [];
 
 //The dice
-//Random rolling of dice
+
 var getDiceRoll = function () {
-  var diceRoll = Math.ceil(Math.random() * 6);
-  console.log(diceRoll);
-  return diceRoll;
-};
-//Make two dice
-var get2DiceRolls = function () {
-  var theDiceRolls = [getDiceRoll(), getDiceRoll()];
-  console.log(theDiceRolls);
-  return theDiceRolls;
+  return Math.ceil(Math.random() * 6);
 };
 
-//The order of the number
-//The order of the number that the player picked
+//Player 1 Dice
+var player01DiceRolls = [getDiceRoll(), getDiceRoll()];
+var player01firstChoice = "" + player01DiceRolls[0] + player01DiceRolls[1];
+var player01secondChoice = "" + player01DiceRolls[1] + player01DiceRolls[0];
 
-var getPlayerOrder = function (numberOrder) {
-  //Calling the 2 dice function
-  var playerPickOrder = get2DiceRolls();
-  var return01 = "" + playerPickOrder[0] + playerPickOrder[1];
-  var return02 = "" + playerPickOrder[1] + playerPickOrder[0];
-  var returnInvalid = "Press 1 or 2.";
-  console.log(playerPickOrder);
-  console.log(return01);
-  console.log(return02);
-  //Checking the numbers and forcing it to be a string
+//Player 2 Dice
+var player02DiceRolls = [getDiceRoll(), getDiceRoll()];
+var player02firstChoice = "" + player02DiceRolls[0] + player02DiceRolls[1];
+var player02secondChoice = "" + player02DiceRolls[1] + player02DiceRolls[0];
 
-  if (numberOrder == "1") {
-    console.log(return01);
-    return return01;
+// Function for when the player selects their choice
+//Player 1 Choice
+var getPlayers01Choice = function (arrangement) {
+  if (arrangement == "1") {
+    player01Number.push(player01firstChoice);
+    return player01firstChoice + ". Player 2 turn now! Click submit.";
   }
-  if (numberOrder == "2") {
-    console.log(return02);
-    return return02;
+  if (arrangement == "2") {
+    player01Number.push(player01secondChoice);
+    return player01secondChoice + ". Player 2 turn now! Click submit.";
   }
-  if (numberOrder != "1" || numberOrder != "2") {
-    return returnInvalid;
-  }
+  return (
+    "Press 1 for " +
+    player01firstChoice +
+    " or 2 for " +
+    player01secondChoice +
+    "."
+  );
 };
 
-var getReturns = function (insert) {
-  var playerOrder = getPlayerOrder(insert);
-  if (insert == "1") {
-    return `You press 1. Your integer is ${playerOrder}`;
+//Player 2 Choice
+var getPlayers02Choice = function (arrangement) {
+  if (arrangement == "1") {
+    player02Number.push(player02firstChoice);
+    return player02firstChoice;
   }
-  if (insert == "2") {
-    return `You press 2. Your integer is ${playerOrder}`;
+  if (arrangement == "2") {
+    player02Number.push(player02secondChoice);
+    return player02secondChoice;
   }
-  if (insert != "1" || insert != "2") {
-    return "Invalid. Only type 1 or 2 in the input box.";
-  }
+  return (
+    "Press 1 for " +
+    player02firstChoice +
+    " or 2 for " +
+    player02secondChoice +
+    "."
+  );
 };
+
 //Main function
 var main = function (input) {
-  var returnMessage = getReturns();
-  var twoDiceRolls = get2DiceRolls();
-  if (gameState == gameDiceRoll) {
-    if (input != "") {
-      gameState = gameDiceRoll
-      return "Click submit only!";
-    }
+  var player01Choice = getPlayers01Choice(input);
+  var player02Choice = getPlayers02Choice(input);
+  if (gameState == gameDiceRollforPlayer1)
     if (input == "") {
-      gameState = gamePickDiceOrder
-      return `Your roll ${twoDiceRolls} respectively. Press 1 or 2 to select the starting the number for your integer. `;
+      gameState = gameUserOrderforPlayer1;
+      return `Dices are ${player01DiceRolls[0]} and ${player01DiceRolls[1]}. Press 1 for ${player01firstChoice} or 2 for ${player01secondChoice} to be your number.`;
+    }
+
+  if (gameState == gameUserOrderforPlayer1) {
+    gameState = gameDiceRollforPlayer2;
+    return player01Choice;
+  }
+
+  if (gameState == gameDiceRollforPlayer2) {
+    gameState = gameUserOrderforPlayer2;
+    return `Dices are ${player02DiceRolls[0]} and ${player02DiceRolls[1]}. Press 1 for ${player02DiceRolls[0]} or 2 for ${player02DiceRolls[1]} to be your first number.`;
+  }
+
+  if (gameState == gameUserOrderforPlayer2) {
+    gameState = compareNumbers;
+    console.log("Player 1" + player01Number);
+    console.log("Player 2" + player02Number);
+    return player02Choice + "<br> So the winner is..";
+  }
+  if (gameState == compareNumbers) {
+    if (player01Number[0] > player02Number[0]) {
+      return "PLAYER 1 WON! Press F5 to restart the game.";
+    }
+    if (player01Number[0] < player02Number[0]) {
+      return "PLAYER 2 WON! Press F5 to restart the game.";
     }
   }
-  if(gameState == gamePickDiceOrder){
 };
-// var popo = ["1", "2"];
-// console.log(popo[0] + popo[1], popo[1]);
-// var main = function (main) {
-//   return popo;
-// }
